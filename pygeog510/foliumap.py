e0a4380b74257d0c9ce6693f59311f4bd9843b6b
lweber89 <lweber89@gmail.com>
2026-09-26 05:32:53 -0500

    Work on Lab 6

--- pygeog510/foliumap.py
+++ pygeog510/foliumap.py
@@ -4,3 +4,12 @@ import folium
 class Map(folium.Map):
     def __init__(self, center=(0, 0), zoom=2, **kwargs):
         super().__init__(location=center, zoom_start=zoom, **kwargs)
+
+    def add_basemap(self, *args, **kwargs):
+        print("Made it to add_basemap")
+
+    def add_layer_control(self, *args, **kwargs):
+        print("Made it to add_layer_control")
+
+    def add_vector(self, *args, **kwargs):
+        print("Made it to add_vector")
