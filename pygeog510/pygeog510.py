235c422dfb5a29dc90eb7f08f5a37114456fa2cf
lweber89 <lweber89@gmail.com>
2026-09-26 15:37:02 -0500

    Continued Lab 6

--- pygeog510/pygeog510.py
+++ pygeog510/pygeog510.py
@@ -24,3 +24,37 @@ class Map(ipyleaflet.Map):
         url = eval(f"ipyleaflet.basemaps.{basemap}").build_url()
         layer = ipyleaflet.TileLayer(url=url, name=basemap)
         self.add(layer)
+
+    def add_geojson(
+        self,
+        data,
+        zoom_to_layer=True,
+        hover_style=None,
+        **kwargs,
+    ):
+        """Adds a GeoJSON layer to the map.
+
+        Args:
+            data (str or dict): The GeoJSON data. Can be a file path (str) or a dictionary.
+            zoom_to_layer (bool, optional): Whether to zoom to the layer's bounds. Defaults to True.
+            hover_style (dict, optional): Style to apply when hovering over features. Defaults to {"color": "yellow", "fillOpacity": 0.2}.
+            **kwargs: Additional keyword arguments for the ipyleaflet.GeoJSON layer.
+
+        Raises:
+            ValueError: If the data type is invalid.
+        """
+
+        import geopandas as gpd
+
+        if hover_style == None:
+            hover_style = {"color": "yellow", "fillOpacity": 0.2}
+
+        gdf = gpd.read_file(data)
+        geojson = gdf.__geo_interface__
+
+        layer = ipyleaflet.GeoJSON(data=geojson, hover_style=hover_style)
+        self.add_layer(layer)
+
+        if zoom_to_layer:
+            bounds = gdf.total_bounds
+            self.fit_bounds([[bounds[1], bounds[0]], [bounds[3], bounds[2]]])
